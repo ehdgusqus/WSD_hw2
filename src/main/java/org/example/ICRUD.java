d45d7517c71003998ebf9d28ad6e7edcf0662f57
ehdgusqus <ehdgusqus55@gmail.com>
2024-10-31 23:56:31 +0900

    update ICRUD

--- src/main/java/org/example/ICRUD.java
+++ src/main/java/org/example/ICRUD.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public interface ICRUD {
     public ShopInform add();
-    public void modify(ArrayList<ShopInform> shopList, int id);
-    public void delete(ArrayList<ShopInform> shopList, int id);
+    public ShopInform modify(ArrayList<ShopInform> shopList, int id);
+    public boolean delete(ArrayList<ShopInform> shopList, int id);
     public void list(ArrayList<ShopInform> shopList);
     public void listPrice(ArrayList<ShopInform> shopList);
     public void search(ArrayList<ShopInform> shopList);
