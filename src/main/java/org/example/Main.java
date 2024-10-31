b83a0ad0fc04e7fb0a2b05e54f04a0fc0b7e4957
ehdgusqus <ehdgusqus55@gmail.com>
2024-11-01 03:04:41 +0900

    update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        ShopManager shopManager = new ShopManager();
-        shopManager.start();
+        DBUtil dbUtil = new DBUtil();
+
+        // ShopManager 시작
+        ShopManager manager = new ShopManager();
+        manager.start();
     }
 }
